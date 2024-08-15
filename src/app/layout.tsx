d83cff2021f4623f0a4ae5a2ee9d487ee6914cf2
factblock-kyle <kyle.lee@factblock.com>
2024-08-15 16:49:59 +0900

    feat: custom cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import CustomCursor from "@/components/core/Cursor";
 
 const ubuntu = Ubuntu({
   weight: ["400", "700"],
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <CustomCursor />
+        {children}
+      </body>
     </html>
   );
 }
