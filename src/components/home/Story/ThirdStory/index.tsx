8262b35fd7cf37133b412cb93ce7eb092653b2ea
factblock-kyle <kyle.lee@factblock.com>
2024-10-17 22:48:35 +0900

    feat: third story layout

--- src/components/home/Story/ThirdStory/index.tsx
+++ src/components/home/Story/ThirdStory/index.tsx
@@ -5,8 +5,21 @@ export const ThirdStory = () => {
   return (
     <div className={S.container}>
       <Flex flexDirection="column">
-        <Text variant={"heading3"}>How I Think, Learn, Work.</Text>
+        <Text variant={"heading3"}>How I Think</Text>
         <Text variant={"heading2"}>Think</Text>
+      </Flex>
+      <Flex gap={12}>
+        <div className={S.featured}>
+          <Flex>
+            <div className={S.featuredImage}>Picture</div>
+          </Flex>
+        </div>
+        <div className={S.grid}>
+          <div>Picture</div>
+          <div>Picture</div>
+          <div>Picture</div>
+          <div>Picture</div>
+        </div>
       </Flex>
     </div>
   );
--- src/components/home/Story/ThirdStory/styles.css.ts
+++ src/components/home/Story/ThirdStory/styles.css.ts
@@ -4,6 +4,25 @@ export const container = style({
   width: "100%",
   background: "white",
   color: "black",
-  height: "fit-content",
   padding: "40px 60px",
+});
+
+export const featured = style({
+  flex: "0 0 33%", // Takes up 33% of the available space
+  backgroundColor: "#f0f0f0", // Example background color
+});
+
+export const grid = style({
+  flex: "0 0 66%", // Takes up 66% of the available space
+  backgroundColor: "#e0e0e0",
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)", // 2 columns
+  gridTemplateRows: "repeat(2, 1fr)", // 2 rows
+  gap: "10px",
+});
+export const featuredImage = style({
+  border: "1px solid black",
+  background: "black",
+  width: "100%",
+  minHeight: "300px",
 });
