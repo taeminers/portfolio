951f640177d43ecad41834219f9da4be9d2edc11
taeminers <taeminers@gmail.com>
2024-10-23 22:39:50 +0900

    feat: medium api to get userId

--- src/components/home/Story/ThirdStory/index.tsx
+++ src/components/home/Story/ThirdStory/index.tsx
@@ -1,26 +1,33 @@
 import Flex from "@/components/core/Flex";
 import * as S from "./styles.css";
 import Text from "@/components/core/Text";
+import { useEffect } from "react";
 export const ThirdStory = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/medium/me", {
+          method: "GET",
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch data from Medium API");
+        }
+        const { data } = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data from Medium API:", error);
+        const errorMessage =
+          error instanceof Error ? error.message : "Unknown error";
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className={S.container}>
       <Flex flexDirection="column">
         <Text variant={"heading3"}>How I Think</Text>
         <Text variant={"heading2"}>Think</Text>
       </Flex>
-      {/* <Flex gap={12}>
-        <div className={S.featured}>
-          <Flex>
-            <div className={S.featuredImage}>Picture</div>
-          </Flex>
-        </div>
-        <div className={S.grid}>
-          <div style={{ background: "red" }}>Picture</div>
-          <div style={{ background: "blue" }}>Picture</div>
-          <div style={{ background: "green" }}>Picture</div>
-          <div style={{ background: "orange" }}>Picture</div>
-        </div>
-      </Flex> */}
       <div className={S.grid}>
         <div style={{ background: "red", width: "40vw", height: "40vh" }}>
           Picture
