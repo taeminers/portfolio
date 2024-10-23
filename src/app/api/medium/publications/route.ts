1d10a5438b6c6f7ed2cae7cc94d6548c38ba756f
taeminers <taeminers@gmail.com>
2024-10-23 23:02:26 +0900

    feat: get my posts from medium

---
+++ src/app/api/medium/publications/route.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export async function GET(req: Request) {
+  if (req.method !== "GET") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+
+  const token = process.env.MEDIUM_TOKEN;
+  const apiEndpoint = process.env.UNOFFICIAL_MEDIUM_API_ENDPOINT;
+  const username = "@notanordinarydev";
+  if (!token || !apiEndpoint || !username) {
+    return new Response("Environment variables not set", { status: 500 });
+  }
+
+  try {
+    const { data: response } = await axios.get(`${apiEndpoint}/${username}`);
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data from Medium API:", error);
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+
+    return new Response(
+      JSON.stringify({ message: "Internal Server Error", error: errorMessage }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
