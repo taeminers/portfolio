aa00bb60801b8b038a0e99202f7c4bda58648936
taeminers <taeminers@gmail.com>
2024-11-21 22:05:25 +0900

    feat: third story double job in single year

--- src/components/home/Story/SecondStory/ThirdJob/index.tsx
+++ src/components/home/Story/SecondStory/ThirdJob/index.tsx
@@ -4,39 +4,53 @@ import * as S from "./styles.css";
 
 export const ThirdJob = () => {
   return (
-    <Flex justify="between" className={S.container}>
-      <Flex flexDirection="column" gap={12}>
-        <Text variant={"heading6"} color={"base-lighter"}>
-          Software Developer
-        </Text>
-        <Flex gap={32} className={S.container}>
-          <Flex flexDirection="column" gap={12}>
-            <Text variant={"body5"} color={"base-lighter"}>
-              LG CNS: Intern
-            </Text>
-            <Text variant={"body6"} color={"base-white"}>
-              - Turning React Project into NextJS
-            </Text>
-            <Text variant={"body6"} color={"base-white"}>
-              - Learnings
-            </Text>
-          </Flex>
-          <Flex flexDirection="column" gap={12}>
-            <Text variant={"body5"} color={"base-lighter"}>
-              Bondit: Frontend Developer
-            </Text>
-            <Text variant={"body6"} color={"base-white"}>
-              - Building Landing Page
-            </Text>
-            <Text variant={"body6"} color={"base-white"}>
-              - Log-in flow for igotin app (flutter)
-            </Text>
+    <Flex justify="between" gap={32}>
+      <Flex className={S.jobContainer} justify="between">
+        <Flex flexDirection="column" gap={12}>
+          <Text variant={"heading6"} color={"base-lighter"}>
+            Software Developer
+          </Text>
+          <Flex justify="between">
+            <Flex flexDirection="column" gap={12} className={S.container}>
+              <Text variant={"body5"} color={"base-lighter"}>
+                LG CNS: Intern
+              </Text>
+              <Text variant={"body6"} color={"base-white"}>
+                - Turning React Project into NextJS
+              </Text>
+              <Text variant={"body6"} color={"base-white"}>
+                - Learnings
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
+        <Text variant={"body6"} color={"base-lighter"}>
+          Jun 2023 - Aug 2023
+        </Text>
       </Flex>
-      <Text variant={"body6"} color={"base-lighter"}>
-        Jun 2023 - Aug 2023
-      </Text>
+      <Flex className={S.jobContainer} justify="between">
+        <Flex flexDirection="column" gap={12}>
+          <Text variant={"heading6"} color={"base-lighter"}>
+            Software Developer
+          </Text>
+          <Flex justify="between">
+            <Flex flexDirection="column" gap={12} className={S.container}>
+              <Text variant={"body5"} color={"base-lighter"}>
+                Bondit: Frontend Developer
+              </Text>
+              <Text variant={"body6"} color={"base-white"}>
+                - Building Landing Page
+              </Text>
+              <Text variant={"body6"} color={"base-white"}>
+                - Log-in flow for igotin app (flutter)
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Text variant={"body6"} color={"base-lighter"}>
+          Aug 2023 - Dec 2023
+        </Text>
+      </Flex>
     </Flex>
   );
 };
--- src/components/home/Story/SecondStory/ThirdJob/styles.css.ts
+++ src/components/home/Story/SecondStory/ThirdJob/styles.css.ts
@@ -3,3 +3,8 @@ import { style } from "@vanilla-extract/css";
 export const container = style({
   minHeight: "130px",
 });
+
+export const jobContainer = style({
+  width: "100%",
+  minHeight: "130px",
+});
