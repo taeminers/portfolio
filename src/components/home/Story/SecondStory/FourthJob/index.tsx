cc490a3219ee1aa01df1cb1d7fe49ccf504dfb55
taeminers <taeminers@gmail.com>
2024-12-15 21:32:00 +0900

    feat: add all logos for jobs

--- src/components/home/Story/SecondStory/FourthJob/index.tsx
+++ src/components/home/Story/SecondStory/FourthJob/index.tsx
@@ -1,25 +1,28 @@
 import Flex from "@/components/core/Flex";
 import Text from "@/components/core/Text";
 import * as S from "./styles.css";
-
+import Image from "next/image";
 export const FourthJob = () => {
   return (
     <Flex justify="center" flexDirection="column">
-      <Flex flexDirection="column" gap={12}>
-        <Text variant={"heading6"} color={"base-lighter"}>
-          Software Developer
-        </Text>
-        <Flex flexDirection="column" gap={12} className={S.container}>
-          <Text variant={"body5"} color={"base-lighter"}>
-            FACTBLOCK: Frontend Developer
+      <Flex gap={32}>
+        <Flex flexDirection="column" gap={12}>
+          <Text variant={"heading6"} color={"base-lighter"}>
+            Software Developer
           </Text>
-          <Text variant={"body6"} color={"base-white"}>
-            - Pitch Deck
-          </Text>
-          <Text variant={"body6"} color={"base-white"}>
-            - Learnings
-          </Text>
+          <Flex flexDirection="column" gap={12} className={S.container}>
+            <Text variant={"body5"} color={"base-lighter"}>
+              FACTBLOCK: Frontend Developer
+            </Text>
+            <Text variant={"body6"} color={"base-white"}>
+              - Pitch Deck
+            </Text>
+            <Text variant={"body6"} color={"base-white"}>
+              - Learnings
+            </Text>
+          </Flex>
         </Flex>
+        <Image src={"/factblock_logo.jpg"} width={50} height={50} alt="grids" />
       </Flex>
       <Text variant={"body6"} color={"base-lighter"}>
         Feb 2024 - current
--- src/components/home/Story/SecondStory/ThirdJob/index.tsx
+++ src/components/home/Story/SecondStory/ThirdJob/index.tsx
@@ -1,51 +1,58 @@
 import Flex from "@/components/core/Flex";
 import Text from "@/components/core/Text";
 import * as S from "./styles.css";
-
+import Image from "next/image";
 export const ThirdJob = () => {
   return (
     <Flex justify="between" gap={32}>
       <Flex className={S.jobContainer} justify="center" flexDirection="column">
-        <Flex flexDirection="column" gap={12}>
-          <Text variant={"heading6"} color={"base-lighter"}>
-            Software Developer
-          </Text>
-          <Flex justify="between">
-            <Flex flexDirection="column" gap={12} className={S.container}>
-              <Text variant={"body5"} color={"base-lighter"}>
-                LG CNS: Intern
-              </Text>
-              <Text variant={"body6"} color={"base-white"}>
-                - Turning React Project into NextJS
-              </Text>
-              <Text variant={"body6"} color={"base-white"}>
-                - Learnings
-              </Text>
+        <Flex gap={32}>
+          <Flex flexDirection="column" gap={12}>
+            <Text variant={"heading6"} color={"base-lighter"}>
+              Software Developer
+            </Text>
+            <Flex justify="between">
+              <Flex flexDirection="column" gap={12} className={S.container}>
+                <Text variant={"body5"} color={"base-lighter"}>
+                  LG CNS: Intern
+                </Text>
+                <Text variant={"body6"} color={"base-white"}>
+                  - Turning React Project into NextJS
+                </Text>
+                <Text variant={"body6"} color={"base-white"}>
+                  - Learnings
+                </Text>
+              </Flex>
             </Flex>
           </Flex>
+          <Image src={"/lg_cns_logo.png"} width={120} height={30} alt="grids" />
         </Flex>
+
         <Text variant={"body6"} color={"base-lighter"}>
           Jun 2023 - Aug 2023
         </Text>
       </Flex>
       <Flex className={S.jobContainer} justify="center" flexDirection="column">
-        <Flex flexDirection="column" gap={12}>
-          <Text variant={"heading6"} color={"base-lighter"}>
-            Software Developer
-          </Text>
-          <Flex justify="between">
-            <Flex flexDirection="column" gap={12} className={S.container}>
-              <Text variant={"body5"} color={"base-lighter"}>
-                Bondit: Frontend Developer
-              </Text>
-              <Text variant={"body6"} color={"base-white"}>
-                - Building Landing Page
-              </Text>
-              <Text variant={"body6"} color={"base-white"}>
-                - Log-in flow for igotin app (flutter)
-              </Text>
+        <Flex gap={32}>
+          <Flex flexDirection="column" gap={12}>
+            <Text variant={"heading6"} color={"base-lighter"}>
+              Software Developer
+            </Text>
+            <Flex justify="between">
+              <Flex flexDirection="column" gap={12} className={S.container}>
+                <Text variant={"body5"} color={"base-lighter"}>
+                  Bondit: Frontend Developer
+                </Text>
+                <Text variant={"body6"} color={"base-white"}>
+                  - Building Landing Page
+                </Text>
+                <Text variant={"body6"} color={"base-white"}>
+                  - Log-in flow for igotin app (flutter)
+                </Text>
+              </Flex>
             </Flex>
           </Flex>
+          <Image src={"/bondit_logo.png"} width={50} height={50} alt="grids" />
         </Flex>
         <Text variant={"body6"} color={"base-lighter"}>
           Aug 2023 - Dec 2023
