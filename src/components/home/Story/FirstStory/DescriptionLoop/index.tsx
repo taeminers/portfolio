431838012a02e509ad43e05856fd7ed3edfbb892
taeminers <taeminers@gmail.com>
2024-12-22 22:07:59 +0900

    fix: word change

--- src/components/home/Story/FirstStory/DescriptionLoop/index.tsx
+++ src/components/home/Story/FirstStory/DescriptionLoop/index.tsx
@@ -7,7 +7,7 @@ export const DescriptionLoop = () => {
     "Creative",
     "Innovative",
     "a Dreamer",
-    'Ready to Grow"',
+    "Ready to Grow",
   ];
 
   const [descriptionArray, setDescriptionArray] = useState(initialArray);
