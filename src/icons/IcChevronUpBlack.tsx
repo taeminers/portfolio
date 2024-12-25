9f42fc06f86fc574146c7e7115cf17ae3a4877da
taeminers <taeminers@gmail.com>
2024-12-25 23:15:22 +0900

    feat: footer layout

---
+++ src/icons/IcChevronUpBlack.tsx
@@ -0,0 +1,21 @@
+export const IcChevronUpBlack = () => {
+  return (
+    <svg
+      className="w-[35px] h-[35px] text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="33"
+      height="33"
+      fill="black"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m16 14-4-4-4 4"
+      />
+    </svg>
+  );
+};
